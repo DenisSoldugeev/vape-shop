57ad7736aaa15eb42dc5cbac76cb7842b5a8dc3f
denis.soldugeev <denis.soldugeev@lectera.com>
2023-10-12 23:55:56 +0400

    feat: Added react-icons

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,19 +1,17 @@
 import { FC } from 'react';
 import Link from 'next/link';
 import Button from 'components/Button';
+import { FaListUl } from 'react-icons/fa6';
 import styles from './Header.module.scss';
+
 export const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div className='container'>
         <div className={styles.wrapper}>
           <div className={styles.btnWrapper}>
-            <Button
-              color='primary'
-              size='medium'
-              outline
-              className={styles.btnCatalog}
-            >
+            <Button color='primary' size='medium' className={styles.btnCatalog}>
+              <FaListUl size={18} />
               Каталог
             </Button>
           </div>
